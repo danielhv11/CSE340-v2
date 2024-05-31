a9cf7095e395cad4b2ed5e514cdca71ca537e451
Daniel Hinojos <daniel.hinojos@kroniatech.com>
2024-05-30 20:20:21 -0500

    fixed errors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@
   //Index Route
   app.use("/", static)
   // Use the baseController.buildHome for the "/" route
-  app.get("/", baseController.buildHome)
+  app.get("/", utilities.handleErrors(baseController.buildHome))
   // Use the inventoryRoute for the "/inv" route
   app.use("/inv", inventoryRoute)
 
@@ -48,9 +48,10 @@ Place after all other middleware
   app.use(async (err, req, res, next) => {
     let nav = await utilities.getNav()
     console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+    if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
     res.render("errors/error", {
       title: err.status || 'Server Error',
-      message: err.message,
+      message,
       nav
     })
   })
