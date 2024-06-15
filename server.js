5a51fdb48e706c7ec9eb4a6e5bcaad0397a1cb44
Daniel Hinojos <daniel.hinojos@kroniatech.com>
2024-06-15 13:11:09 -0500

    week8

--- server.js
+++ server.js
@@ -12,7 +12,12 @@
   const static = require("./routes/static")
   const baseController = require("./controllers/baseController")
   const inventoryRoute = require("./routes/inventoryRoute")
+  const accountRoute = require("./routes/accountRoute")
   const utilities = require("./utilities");
+  const session = require("express-session")
+  const pool = require('./database/')
+  const bodyParser = require("body-parser")
+  
   
   /************************
    *View Engine and Templates
@@ -20,14 +25,42 @@
   app.set("view engine", "ejs")
   app.use(expressLayouts)
   app.set("layout","./layouts/layout")//not at views root")
-  
-  
+
+  /* ***********************
+    Middleware
+ *************************/
+  app.use(session({
+      store: new (require('connect-pg-simple')(session))({
+        createTableIfMissing: true,
+        pool,
+      }),
+      secret: process.env.SESSION_SECRET,
+      resave: true,
+      saveUninitialized: true,
+      name: 'sessionId',
+    }))
+
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+    // Express Messages Middleware
+  app.use(require('connect-flash')())
+  app.use(function(req, res, next){
+    res.locals.messages = require('express-messages')(req, res)
+    next()
+  })
+
+
   //Index Route
   app.use("/", static)
   // Use the baseController.buildHome for the "/" route
   app.get("/", utilities.handleErrors(baseController.buildHome))
   // Use the inventoryRoute for the "/inv" route
   app.use("/inv", inventoryRoute)
+  // Use the accountRoute for the "/account" route
+  app.use("/account", accountRoute)
 
   app.use(async (req, res, next) => {
     next({ status: 404, message: "Page not found, sorry" })
